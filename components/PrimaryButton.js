373607f866ec62df3bc7b1bfe53dcb1a6c669588
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-09-25 17:19:52 +0530

    add custom primarybutton

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
