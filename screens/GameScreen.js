f4ddbf298118836bbb029828c228fd4e90fb3ad7
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-09-27 12:35:01 +0530

    implement generate random number function

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,16 @@
 import { Text, View, StyleSheet } from "react-native";
 import Title from "../components/Title";
 
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
 const GameScreen = () => {
   return (
     <View style={styles.screen}>
@@ -10,7 +20,7 @@ const GameScreen = () => {
         <Text>Higher or Lower</Text>
         {/* +- */}
       </View>
-      {/* <View>Log Rounds</View> */}
+      {/* <View>Log  Rounds</View> */}
     </View>
   );
 };
